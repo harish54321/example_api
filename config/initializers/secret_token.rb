aadcb349f77d385f6ce24df346b638f0bc74e91e
Harish Karumanchi <harishkarumanchi@Harishs-MacBook-Pro.local>
2017-01-17 15:29:39 -0600

    Initializes the projec t

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ExampleApi::Application.config.secret_key_base = 'c0e20e225c76d8ccb43879abbd2327f312595584d3575bd26191f47899458a0e8cd1932c2b671cd1ba22a1dc09d3cac06cee368b916b6d3e85bf1f12274c7216'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ExampleApi::Application.config.session_store :cookie_store, key: '_example_api_session'
